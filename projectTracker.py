4cf17d3fa8537ff120b7495ac886c947276f7b13
James Wilde <contact@jameswilde.uk>
2024-08-07 22:40:06 +0100

    added csv handler class / Window config

--- projectTracker.py
+++ projectTracker.py
@@ -1,22 +1,57 @@
-import tkinter as tk
+import tkinter as tk # Primary GUI Library
 from tkinter import messagebox
 from tkinter import ttk
+import os # Used to check if the csv file is present
 import re
-import csv
+import csv #used to read and write from the csv file
 
 class myApp(tk.Tk):
-    def __init__(self):
-            super().__init__()
-            self.geometry("500x400")
-            self.title("Test App")
-            self.config(bg = "skyblue")
+     def __init__(self):
+          super().__init__()
+          self.geometry("500x400")
+          self.title("Test App")
+          self.config(bg = "skyblue")
+          self.data = csvHandler()
+          self.data.read()
+          self.data.get()
 
 
-class editorScreen(tk.Frame):
+class mainScreen(ttk.Frame):
+     def __init__(self, data):
+          super().__init__()
+          l1 = ttk.Label(self, text="test")
+          l1.pack()
+
+class editorScreen(tk.Toplevel):
      pass
 class csvHandler():
-     pass
+     def __init__(self):
+          self.data=[]
+          self.headers=[]
+          self.filepath = "allocation.csv"
+          pass
+     def read(self):
+          if not os.path.isfile(self.filepath):
+               print(f"Error: File '{self.filepath}' does not exist.")
+               return
+          try:
+               with open(self.filepath, newline='') as infile:
+                    reader = csv.reader(infile)
+                    self.headers = next(reader)
+                    self.data = list(reader)
+          except FileNotFoundError:
+               print(f"Error: File '{self.filepath}' not found.")
+          except csv.Error as e:
+               print(f"Error: Could not read CSV file. {e}")
+          except Exception as e:
+               print(f"An unexpected error occurred: {e}")
+     def write(self):
+          pass
+     def add_row(self, to_add):
+          self.data.append(to_add)
+     def get(self):
+          return self.data
 
 if __name__=="__main__":
-    app= myApp()
-    app.mainloop()
+     app= myApp()
+     app.mainloop()
