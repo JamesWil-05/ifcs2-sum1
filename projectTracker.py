69477a794e52c10c7149325d023f8e32401e6580
James Wilde <contact@jameswilde.uk>
2024-08-11 17:00:49 +0100

    Added comments and docstrings

--- projectTracker.py
+++ projectTracker.py
@@ -1,13 +1,16 @@
 import tkinter as tk  # Primary GUI Library
-from tkinter import messagebox
-from tkinter import ttk
+from tkinter import messagebox # import the tk message box feature for errors
+from tkinter import ttk # import ttk package for treeview widget
 import os  # Used to check if the csv file is present
 import re # used for format validation
 import csv  # used to read and write from the csv file
 
 
 class ResourceManager(tk.Tk):
-    '''Resource Manager is a class that contains the main landing screen for this application, It displays the csv data in a clear format, '''
+    '''Resource Manager is a class that contains the main landing screen for this application, It contains the configs for the window itself,
+    as well as the widgets in use on the main screen.
+    The Purpose of the window is to display the data loaded from the csv in the treeview widget self.tree.
+    The data is loaded into this window using the csv Handeler Object'''
     def __init__(self):
         super().__init__()
         self.geometry("800x350")
@@ -17,9 +20,10 @@ class ResourceManager(tk.Tk):
         self.data.read()
 
 
-        self.button_frame = tk.Frame(self, bg="skyblue")
-        self.button_frame.pack(fill= tk.BOTH)
+        self.button_frame = tk.Frame(self, bg="skyblue") #Button Frame is used to display the control buttons side by side, all over the tree frame
+        self.button_frame.pack(fill= tk.BOTH) # Packs the Frame, ensuring it expands into the available space
 
+        #Instantiate and pack each button
         self.add_button = tk.Button(self.button_frame, text="Add Row", command=self.add_row, bg="lightgreen")
         self.add_button.pack(padx=10, pady=10, side="left")
 
@@ -29,6 +33,7 @@ class ResourceManager(tk.Tk):
         self.save_button = tk.Button(self.button_frame, text="Save Changes and exit", command=self.save_exit, bg="snow")
         self.save_button.pack(padx=10, pady=10, side="right")
 
+        #Instansiate and pack the tree frame, used to contain the data table
         self.tree_frame = tk.Frame(self)
         self.tree_frame.pack(fill= tk.BOTH, expand= True)
 
@@ -39,78 +44,85 @@ class ResourceManager(tk.Tk):
         self.explain_label.pack(pady=10)
 
         self.fill_table()
-    def save_exit(self):
+    def save_exit(self): # This triggers when self.save_button is triggered, it saves the new data to the csv file, then quits the program
         self.data.write()
         quit()
     def fill_table(self):
-        if self.tree:
+        '''Method for Filling the treeview with up to date data.
+         '''
+        if self.tree: # Removes previous table, to replace with new data
             self.tree.destroy()
-        self.tree = ttk.Treeview(self.tree_frame)
+        self.tree = ttk.Treeview(self.tree_frame)  #Instansiates the new treeview object, setting the frame to tree_frame
         self.tree.pack(fill=tk.BOTH, expand=True)
 
-        self.tree.config(columns= self.data.headers,
+        self.tree.config(columns= self.data.headers, #Names columns using headers from the data object
                          show = "headings")
 
-        for col in self.tree["columns"]:
+        for col in self.tree["columns"]: # Sets each heading equal to the repective column name
             self.tree.heading(col, text=col)
-        for row in self.data.data:
+        for row in self.data.data: # Inserts data into table
             self.tree.insert("", "end", values=row)
 
-        self.tree.bind("<Double-1>", self.edit_val)
-    def edit_val(self, event):
-        row = self.tree.selection()[0]
-        col = self.tree.identify_column(event.x)
-        column_index = int(col.replace("#", "")) - 1
-        column_name = self.tree["columns"][column_index]
-        old_value = self.tree.item(row, "values")[column_index]
-        editorScreen(self, row, column_index, column_name, old_value)
+        self.tree.bind("<Double-1>", self.edit_val) # Enables action to update a value using double left click
+    def edit_val(self, click): # A Method used to collect information for the editor screen, used for changing a value
+        row = self.tree.selection()[0] # Gets row id
+        col = self.tree.identify_column(click.x) # gets column id
+        column_index = int(col.replace("#", "")) - 1 # converts column id into index format for csv handler
+        column_name = self.tree["columns"][column_index] # gets name of column for error checking
+        old_value = self.tree.item(row, "values")[column_index] # gets current value, to autofill the textbox in the new window with it
+        editorScreen(self, row, column_index, column_name, old_value) # instantiates the editor screen, setting main window as parent
 
-    def delete_row(self):
-        if self.tree.selection():
-            row = self.tree.selection()[0]
+    def delete_row(self): # A method used to remove a row from the data set
+        if self.tree.selection(): # checks the the user has selected a row to delete
+            row = self.tree.selection()[0]# gets the first row selected (in case multiple were selected)
             row_index = self.tree.index(row)
-            self.data.delete(row_index)
-            self.fill_table()
-        else:
+            self.data.delete(row_index)# deletes the row from the main data set using row index
+            self.fill_table() # updates table to reflect change
+        else: # if not cancel operation with error popup
             messagebox.showwarning(title="Error", message="Select a row first")
-    def add_row(self):
+    def add_row(self): # method to open add row screen
         addScreen(self)
 
 class editorScreen(tk.Toplevel):
-    def __init__(self, parent, row, col, col_name, old_value):
+    '''This class is used to create an editor screen to edit a specific cell. it uses previously collected data about the selected value to preform error checks
+    on the new proposed value.'''
+    def __init__(self, parent, row, col, col_name, old_value): # initialise the new window
         super().__init__(parent)
-        self.title("Edit Value")
+        self.title(f"Edit {col_name}")
         self.geometry("300x200")
         self.config(bg="skyblue")
-        self.parent = parent
+        self.parent = parent # sets parent of window to be main window
         self.row = row
         self.col = col
         self.col_name = col_name
 
-        self.edit_label = (tk.Label(self, text="Edit value:", bg="skyblue"))
+        self.edit_label = (tk.Label(self, text=f"Edit {col_name}:", bg="skyblue")) # A Label explaining what this window is for
         self.edit_label.pack(pady=10)
-        self.new_value_input = tk.Entry(self)
-        self.new_value_input.pack(pady=10)
-        self.new_value_input.insert(0, old_value)
 
-        self.save_button = tk.Button(self, text="Save", command=self.save_edit, bg="lightgreen")
+        self.new_value_input = tk.Entry(self) # the input box for the new value to be inserted
+        self.new_value_input.pack(pady=10)
+        self.new_value_input.insert(0, old_value) # sets the input field text to the current value, in order the make it clear what the user is changing
+
+        self.save_button = tk.Button(self, text="Save", command=self.save_edit, bg="lightgreen") # Button to trigger checks on the new value, if they pass then the change is applied
         self.save_button.pack(pady=10)
     def save_edit(self):
-        new_value = self.new_value_input.get()
-        if self.parent.data.value_check(new_value, self.col_name):
-            self.parent.tree.set(self.row, column=self.parent.tree["columns"][self.col], value=new_value)
-            self.parent.data.update(self.parent.tree)
-        self.destroy()
-
+        new_value = self.new_value_input.get() # gets the new input
+        if self.parent.data.value_check(new_value, self.col_name): #runs checks based off the column name ot determine the required checks
+            self.parent.tree.set(self.row, column=self.parent.tree["columns"][self.col], value=new_value) #updates tree with new value
+            self.parent.data.update(self.parent.tree) #uses tree to update dataset
+        self.destroy() # close window once process is finished
 
 class addScreen(tk.Toplevel):
-    def __init__(self, parent):
+    '''Class for window for adding a new row to the table. This class allows a user to input data for each field,
+    runs check on each field, and if they pass, the dataset is appended with the new value'''
+    def __init__(self, parent):# initialise the new window
         super().__init__(parent)
         self.title("Add Row")
         self.geometry("300x400")
         self.config(bg="skyblue")
         self.parent = parent
 
+        #Each input box is creates along side a label describing what is required for each input
         self.name_label = tk.Label(self, text="Name:", bg="skyblue")
         self.name_input = tk.Entry(self)
         self.name_label.pack()
@@ -131,36 +143,39 @@ class addScreen(tk.Toplevel):
         self.time_label.pack()
         self.time_input.pack(pady=10)
 
-        self.save_button = tk.Button(self, text="Save", command=self.save_edit, bg="lightgreen")
+        self.save_button = tk.Button(self, text="Save", command=self.save_edit, bg="lightgreen") # A button to trigger the value checks and saving of the data
         self.save_button.pack(pady=10)
     def save_edit(self):
-        if self.parent.data.value_check(self.name_input.get(), "name"):
+        if self.parent.data.value_check(self.name_input.get(), "name"): # each value is tested, and is all pass then the data is added
             if self.parent.data.value_check(self.project_input.get(), "project"):
                 if self.parent.data.value_check(self.name_input.get(), "name"):
                     if self.parent.data.value_check(self.time_input.get(), "time allocated"):
-                        self.parent.data.add([self.name_input.get(),self.project_input.get(),self.role_input.get(),self.time_input.get()])
-                        self.parent.fill_table()
-
-        self.destroy()
-
+                        self.parent.data.add([self.name_input.get(),self.project_input.get(),self.role_input.get(),self.time_input.get()]) # adds data to csv handeler
+                        self.parent.fill_table() # refreshes the table with new data
+        self.destroy() # closes the add row window
 
 class csvHandler():
+    '''A class used as a ultility for managing the csv data throughout the program
+     This class is used to read and write the data to the csv file as well as hold the data referenced by the treeview widget,
+     including fuctions to append, update and delete data from this dataset
+     additionally this class is used to check a proposed new value to be added to the csv'''
     def __init__(self):
-        self.data = []
-        self.headers = []
-        self.filepath = "allocations.csv"
-        pass
-    def normalize(self, value):
+        self.data = [] # this will hold the referenced data
+        self.headers = [] # this holds the headers for each column of the table
+        self.filepath = "allocations.csv" # this is the file that will be read and written to
+    def normalize(self, value): # This normalises a value  (used for both the headers and data types
         return value.strip().lower()
     def value_check(self, value, type):
+        '''This Function is used to call other funcions based on the type it is called with, the value then goes through several checks.
+        If the value fails any check then a message box is triggered and the function returns false, informing the caller not to make the proposed changes'''
         type_normalized = self.normalize(type)
         try:
-            match (type_normalized):
+            match (type_normalized): # Switch statement for each value heading
                 case "name":
                     if self.precence_check(self.normalize(value)):
                         if self.length_check(value):
                             if self.name_format_check(self.normalize(value)):
-                                return True
+                                return True # if all checks pass return true
                             else:
                                 messagebox.showerror(title="Pattern Error",
                                                      message="The Name should contain only Letters, hyphens, apostrophes")
@@ -213,57 +228,55 @@ class csvHandler():
                 case _:
                     messagebox.showerror(title="Error", message="Value type unknown, Checks fail")
                     return False
-        except Exception as e:
+        except Exception as e: # incase an unknown error occurs during checks like value error, will return false to caller
             messagebox.showerror(title="Error",
                                  message=f"An Unknown error has occured: {e}")
             return False
-    def precence_check(self, value):
+    def precence_check(self, value): # checks is there is a value stored in the variable
         if value:
             return True
         return False
-    def length_check(self, value):
+    def length_check(self, value):# checks the value length is within justified range
         return 2<len(value)<=35
-    def name_format_check(self, value):
+    def name_format_check(self, value): # checks the name only contains alphabetic characters as well as some punctuation included in names
         pattern = re.compile(r"^[A-Za-z '-]+$")
         return  bool(pattern.match(value))
     def percentage_check(self, value):
-        pattern = re.compile(r"\b(80|[1-7]?[0-9])\b|\b(80|[1-7]?[0-9]%)\b")
+        pattern = re.compile(r"\b(80|[1-7]?[0-9])\b|\b(80|[1-7]?[0-9]%)\b") # checks if the number is a number between 0 and 80 as the percent should be between these two. Also checks for and allows the inclusion of a %
         return bool(pattern.match(value))
     def read(self):
-        if not os.path.isfile(self.filepath):
+        if not os.path.isfile(self.filepath): # checks if file exists
             print("error")
             messagebox.showinfo(message=f"Error: File '{self.filepath}' does not exist.")
             return
         try:
-            with open(self.filepath, newline='') as infile:
+            with open(self.filepath) as infile: # opens file to be read
                 reader = csv.reader(infile)
-                self.headers = next(reader)
-                self.data = list(reader)
+                self.headers = next(reader)# sets headers from first line of file
+                self.data = list(reader)# converts each line of the file, bar the first, into a list
         except Exception as e:
             messagebox.showerror(f"An unexpected error occurred: {e}")
 
     def write(self):
-        with open(self.filepath, 'w', newline='') as outfile:
+        with open(self.filepath, 'w') as outfile: # opens file to overwrite
             writer = csv.writer(outfile)
-            writer.writerow(self.headers)
-            writer.writerows(self.data)
-        pass
+            writer.writerow(self.headers) # writes headers as first line
+            writer.writerows(self.data) # writes updated data to file
 
-    def add(self, to_add):
+    def add(self, to_add): # function to add a row to data
         self.data.append(to_add)
 
     def delete(self, index):
-        self.data.pop(index)
+        self.data.pop(index) # removes row at selected index from data
 
-    def update(self, tree):
-        self.data=[]
-        for item in tree.get_children():
+    def update(self, tree): # function for overwriting data with updated version
+        self.data=[] # sets current data to none
+        for item in tree.get_children(): # gets all rows from table
             values = tree.item(item)["values"]
-            self.data.append(values)
-    def get(self):
+            self.data.append(values) # inserts rows into table
+    def get(self): # simple function to send data to caller
         return self.data
 
-
 if __name__ == "__main__":
-    app = ResourceManager()
+    app = ResourceManager() # instantiates main app
     app.mainloop()
